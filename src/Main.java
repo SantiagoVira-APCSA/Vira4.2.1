57c1fff434e9d3c757a5779e53723dfc513002bc
SantiagoVira <santiagovira06@gmail.com>
2022-11-10 12:10:20 -0500

    Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringLoops loops = new StringLoops();
+        System.out.println(loops.countCharacters("a", "Apple and banana"));
+        System.out.println(loops.countCharacters("A", "Apple and banana"));
+        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+        System.out.println(loops.reverseString("hello!"));
+        System.out.println(loops.reverseString("Apples and bananas"));
+
+
     }
 }
