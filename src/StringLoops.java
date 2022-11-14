23fc25b0f75b869033f89e3a55266db713e6506b
SantiagoVira <santiagovira06@gmail.com>
2022-11-14 16:29:32 -0500

    More code

--- src/StringLoops.java
+++ src/StringLoops.java
@@ -41,4 +41,66 @@ public class StringLoops {
         }
         return newString;
     }
+
+    public String reverseString2(String origString) {
+        /* to be implemented */
+        String newString = "";
+        for (int i = 0; i < origString.length(); i++) {
+            newString = origString.substring(i, i + 1) + newString;
+        }
+        return newString;
+    }
+
+    public int countString(String searchString, String origString) {
+        /* to be implemented */
+        int idx = origString.indexOf(searchString);
+        int count = 0;
+        while (idx != -1) {
+            origString = origString.substring(idx + 1);
+            idx = origString.indexOf(searchString);
+            count++;
+        }
+        return count;
+    }
+
+    public String replaceCharacter(String searchChar, String origString, String replaceChar) {
+        /* to be implemented */
+        int idx = origString.indexOf(searchChar);
+        while (idx != -1) {
+            origString = origString.replace(searchChar, replaceChar);
+            idx = origString.indexOf(searchChar);
+        }
+        return origString;
+    }
+
+    public boolean isPalindrome(String myString) {
+        /* to be implemented */
+        System.out.println(myString.replace(" ", "") + " | " + reverseString(myString.replace(" ", "")));
+        return myString.replace(" ", "") == reverseString(myString.replace(" ", ""));
+    }
+
+
+    public String removeString(String searchString, String origString) {
+        /* to be implemented */
+        int idx = origString.indexOf(searchString);
+        while (idx != -1) {
+            origString = origString.replace(searchString, "");
+            idx = origString.indexOf(searchString);
+        }
+        return origString;
+    }
+
+
+    public void multiPrint(String toPrint, int num) {
+        /* to be implemented */
+        System.out.print("[");
+
+        if (num > 0) {
+            int finalLen = num * (toPrint.length() + 1) - 1;
+            System.out.print((toPrint + " ").repeat(num).substring(0, finalLen));
+        }
+        System.out.println("]");
+    }
+
+
 }
